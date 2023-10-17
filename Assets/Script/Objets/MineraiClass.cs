99e45bd3086fbcf305a2886b1903e6916075e306
Saltmon69 <91060487+Saltmon69@users.noreply.github.com>
2023-10-17 14:44:53 +0200

    GenRdmCrit

--- Assets/Script/Objets/MineraiClass.cs
+++ Assets/Script/Objets/MineraiClass.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class MineraiClass : MonoBehaviour
@@ -11,23 +12,51 @@ public class MineraiClass : MonoBehaviour
 
     public int mineraiLife;
 
-    public int critMultiplicator = 3;
+    public int critMultiplicator = 1;
     
-    
-    
+    [SerializeField] GameObject critGameObject;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            CritPointCreation();
+        }
+    }
+
     public void takeDamage(int damage)
     {
         if (damage > mineraiLife)
         {
             damage = mineraiLife;
         }
-        mineraiClass.quantite += (int)(damage*mineraiClass.rendement);
-        mineraiLife -= damage;
+        mineraiClass.quantite += (int)(damage * critMultiplicator * mineraiClass.rendement);
+        mineraiLife -= damage * critMultiplicator;
     }
 
     public void DestroyGameObject()
     {
         //Mettre gamefeel
         Destroy(gameObject);
+    }
+    
+    public void CritPointCreation()
+    {
+        Debug.Log("CritPointCreation");
+        
+        int critPointNumber = Random.Range(1, 6);
+        for (int i = 0; i < critPointNumber; i++)
+        {
+            float rdmNegPos = Random.Range(-0.60f, -0.65f);
+            float rdmPosPos = Random.Range(0.60f, 0.65f);
+            GameObject critPoint = Instantiate(critGameObject, transform);
+            critPoint.transform.localPosition = new Vector3(Random.Range(rdmNegPos, rdmPosPos), Random.Range(rdmNegPos, rdmPosPos), Random.Range(rdmNegPos, rdmPosPos));
+            
+        }
     }
 }
